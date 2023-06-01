1ac85641f759f5f412f2f11357faabdca928131b
2TomL <lamerstom.123@gmail.com>
2023-06-01 15:30:17 +0200

    Design Patterns Deel 1 en 2

--- src/be/intecbrussel/Deel2/StrategyPattern/BubbleSortAlgorithm.java
+++ src/be/intecbrussel/Deel2/StrategyPattern/BubbleSortAlgorithm.java
@@ -12,5 +12,6 @@ public class BubbleSortAlgorithm implements ArraySortingAlgorithm{
                 }
             }
         }
+        return array;
     }
 }
--- src/be/intecbrussel/Deel2/StrategyPattern/MainApp.java
+++ src/be/intecbrussel/Deel2/StrategyPattern/MainApp.java
@@ -4,6 +4,15 @@ import java.util.Arrays;
 
 public class MainApp {
     public static void main(String[] args) {
+        int[] array = {5, 3, 2, 1,5, 4,8,7};
+        //gebruik BubbleSort
+        ArraySorter bubbleSorter = new ArraySorter(new BubbleSortAlgorithm());
+        int[] sortedArray = bubbleSorter.sortArray(array);
+        System.out.println("Bubble Sort: " + Arrays.toString(sortedArray));
 
+        //gebruik MergeSort
+        ArraySorter mergeSorter = new ArraySorter(new MergeSortAlgorithm());
+        sortedArray = mergeSorter.sortArray(array);
+        System.out.println("Merge Sort: " + Arrays.toString(sortedArray));
     }
 }
