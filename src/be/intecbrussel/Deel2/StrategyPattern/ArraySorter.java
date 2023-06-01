94502cd8334097c2658b73cc2bb867f0a42ab6bb
2TomL <lamerstom.123@gmail.com>
2023-06-01 15:38:34 +0200

    Designer Patterns deel 1 & 2

--- src/be/intecbrussel/Deel2/StrategyPattern/ArraySorter.java
+++ src/be/intecbrussel/Deel2/StrategyPattern/ArraySorter.java
@@ -2,7 +2,7 @@ package be.intecbrussel.Deel2.StrategyPattern;
 
 public class ArraySorter {
     private ArraySortingAlgorithm sortingAlgorithm;
-
+// vette shit, omdat m niet saved ff dit erbij
     public ArraySorter(ArraySortingAlgorithm sortingAlgorithm) {
         this.sortingAlgorithm = sortingAlgorithm;
     }
